bbd5f1163372c707dd17a9acaa8eca7b4be40f93
함민서 <113820366+miinns@users.noreply.github.com>
2022-12-01 01:20:18 +0900

    Update Program.cs

--- EnumType/Program.cs
+++ EnumType/Program.cs
@@ -6,7 +6,7 @@ class EnumTypeApp
     {
         Color c = Color.Red;
         c++;
-        int i = (int)c; //정
+        int i = (int)c; //
         Console.WriteLine("Cardinality of " + c + " = " + i);
     }
 }
